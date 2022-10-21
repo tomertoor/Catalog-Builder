73d6d5e57d2764dbdfc23f704a05c9e95e8dc41e
RegevMyers <regev.myers@gmail.com>
2022-10-21 11:45:26 +0300

    Tactic parsing done

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
 import mitreattack.attackToExcel.attackToExcel as attackToExcel
 import mitreattack.attackToExcel.stixToDf as stixToDf
+import json
+
+
+def parse_tactics(t):
+    
+    # this is the json we will return
+    r: json = {}
+
+    # for each tactic
+    for row in t.index:
+        str(row)
+
+        j: json = json.loads(t.to_json())
+        
+        # this is the json for the tactic in the format we want
+        row_json: json = {}
+
+        for field in j:
+            if field != "ID":
+                row_json[field] = j[field][row]
+        
+        # adding the tactic to r with the tactic ID as the key
+        r[j["ID"][row]] = row_json
+        
+    return r
+
 
 # download and parse ATT&CK STIX data
 data = attackToExcel.get_stix_data("enterprise-attack")
-# get Pandas DataFrames for techniques
-techniques_data = stixToDf.techniquesToDf(data, "enterprise-attack") 
 
-techniques_df = techniques_data["techniques"]
+# get Pandas DataFrames for tactics & techniques
+tactics_df = stixToDf.tacticsToDf(data)["tactics"]
+techniques_df = stixToDf.techniquesToDf(data, "enterprise-attack")["techniques"]
 
-print("Tactics list: \n", stixToDf.tacticsToDf(data)["tactics"].sort_values("ID"))
-technique = input("Enter technique ID:")
-
-technique = techniques_df[techniques_df["ID"].str.contains(technique)]["name"].tolist()
-print("Technqiue name: ", technique[0])
-for sub_technique in technique[1:]:
-    print("Sub-technique: ", sub_technique)
+print(parse_tactics(tactics_df[["ID", "name", "last modified", "version"]]))
